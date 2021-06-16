bac494ee0534bb413043213a8e89e791e5fd4077
schielke <alexandre@codezeus.com.br>
2021-06-16 02:34:14 -0300

    incluindo link para gerador de cpf válido

--- src/App.vue
+++ src/App.vue
@@ -86,6 +86,7 @@
             <button type="submit" class="btn btn-success">Cadastrar novo cliente</button>
           </div>
         </form>
+        <p>gerador de cpf válido: <a href="https://www.4devs.com.br/gerador_de_cpf" target="_blank" >clique aqui</a></p>
       </section>
 
     </div>
